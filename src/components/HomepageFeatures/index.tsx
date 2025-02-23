28c1e4a2b6a4d1f1e31270bd7dbdba0e4a014e89
przemyslvw <przemyslvw@gmial.com>
2025-02-23 16:51:16 +0100

    aktualizacja grafik frontpage

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -11,32 +11,29 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: 'Bezpieczeństwo IT',
+    Svg: require('@site/static/img/undraw_security.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Zdobądź wiedzę z zakresu bezpieczeństwa sieciowego, webowego i cyberbezpieczeństwa. Poznaj techniki ochrony danych i zabezpieczania aplikacji.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: 'Automatyzacja i Programowanie',
+    Svg: require('@site/static/img/undraw_programming.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Naucz się automatyzować procesy, pisać testy end-to-end (Playwright) i realizować projekty z wykorzystaniem ESP32 oraz Raspberry Pi.
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: 'Tworzenie Aplikacji Internetowych',
+    Svg: require('@site/static/img/undraw_web_development.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Projektowanie i tworzenie nowoczesnych stron oraz aplikacji webowych z wykorzystaniem Angulara, Firebase i TypeScript. Od pomysłu po wdrożenie.
       </>
     ),
   },
