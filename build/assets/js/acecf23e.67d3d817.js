e085e7e91778c09346dc2665298d7a31b9fe9b59
przemyslvw <przemyslvw@gmial.com>
2025-02-23 14:21:37 +0100

    build

---
+++ build/assets/js/acecf23e.67d3d817.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_page=self.webpackChunkmy_page||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/blog","blogTitle":"Blog","authorsListPath":"/blog/authors"}')}}]);
