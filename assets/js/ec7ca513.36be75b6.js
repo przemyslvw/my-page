8da6574ebb26257ae58d37751f76187bd9768271
przemyslvw <przemyslvw@users.noreply.github.com>
2025-02-25 23:22:10 +0000

    deploy: 6eb0e02ee4f395e6049b3e61f721e9cc17f82971

---
+++ assets/js/ec7ca513.36be75b6.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_page=self.webpackChunkmy_page||[]).push([[8667],{5300:a=>{a.exports=JSON.parse('{"tag":{"label":"Atuin","permalink":"/blog/tags/atuin","allTagsPath":"/blog/tags","count":1,"unlisted":false},"listMetadata":{"permalink":"/blog/tags/atuin","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
