0157f718739eba1af8accc19d8aeb8c642a4173f
przemyslvw <przemyslvw@users.noreply.github.com>
2025-03-09 21:19:57 +0000

    deploy: 0df74e280249b9116a8000ff6660e86a61a5a1b6

---
+++ assets/js/c15d9823.3aae3dec.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_page=self.webpackChunkmy_page||[]).push([[8146],{9328:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/blog","page":1,"postsPerPage":10,"totalPages":2,"totalCount":12,"nextPage":"/blog/page/2","blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
