0681884ed829f4c68a9a20ddbb78289bfa7fd9d3
przemyslvw <przemyslvw@users.noreply.github.com>
2025-11-04 13:30:19 +0000

    deploy: 318fe406c70f89c4ec2f6a3608ccb7b8308890ea

---
+++ assets/js/59e1d9d8.18125c7d.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_page=self.webpackChunkmy_page||[]).push([[60954],{92263:a=>{a.exports=JSON.parse('{"tag":{"label":"kurs","permalink":"/blog/tags/kurs","allTagsPath":"/blog/tags","count":1,"unlisted":false},"listMetadata":{"permalink":"/blog/tags/kurs","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
