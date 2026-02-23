f2ea84fe37c0d32b2dcfcc116b491cc5c94506ee
przemyslvw <przemyslvw@users.noreply.github.com>
2026-02-23 19:41:04 +0000

    deploy: b267c827d7f4fa677c536043a3c06bb769c20284

---
+++ assets/js/52e97b50.5ddf2185.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_page=self.webpackChunkmy_page||[]).push([[58880],{98360:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/blog/page/8","page":8,"postsPerPage":10,"totalPages":11,"totalCount":109,"previousPage":"/blog/page/7","nextPage":"/blog/page/9","blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
